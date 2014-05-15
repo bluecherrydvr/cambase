9cc80d362b5de013a4180a4f66642fbb0bed7247
Milos Mosic <mosic.milos@gmail.com>
2014-05-15 14:44:01 +0100

    Added minor fix for the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @data = []
     @images = Image.last(24).reverse
-    @images.each do |image|
-      owner = image.owner_type.classify.constantize.find(image.owner_id)
-      @data.push(owner)
-    end
   end
   def about_cambase
   end
