d3348d78ee97e13504ae89df7cef418cf0ba2fa9
Milos Mosic <mosic.milos@gmail.com>
2014-06-02 12:10:32 +0200

    Added md5 fingerprint to documents

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140523131253) do
+ActiveRecord::Schema.define(version: 20140530144051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20140523131253) do
     t.datetime "file_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "file_fingerprint"
   end
 
   create_table "images", force: true do |t|
