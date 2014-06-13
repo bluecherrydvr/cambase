84fd4d604b7379453c56b4a85609d4e9d91950a6
Milos Mosic <mosic.milos@gmail.com>
2014-06-13 13:40:53 +0100

    Fixed error on redirection after failed login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,7 @@ class ApplicationController < ActionController::Base
     # store last url - this is needed for post-login redirect to whatever the user last visited.
     if (request.fullpath != "/users/sign_in" &&
         request.fullpath != "/users/sign_up" &&
+        request.fullpath != "/users" &&
         request.fullpath != "/users/password" &&
         request.fullpath != "/users/sign_out" &&
         !request.xhr?) # don't store ajax calls
