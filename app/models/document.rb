9c443848260f0a55734985d09a3d552b41d37105
Milos Mosic <mosic.milos@gmail.com>
2014-06-16 14:53:29 +0100

    Fixed file fingerprint implementation

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,5 +2,5 @@ class Document < ActiveRecord::Base
   belongs_to :owner, polymorphic: true
   has_attached_file    :file
   validates_attachment :file, presence: true, content_type: { content_type: /\A.*\Z/ }
-  validates :file_fingerprint, uniqueness: { :message => "already exists in the database." }
+  validates :file_fingerprint, uniqueness: { scope: :owner_id, :message => "already exists in the database." }
 end
