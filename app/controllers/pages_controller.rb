2d418eeb9e705f11f88f0489cf0a00cca8947747
Milos Mosic <mosic.milos@gmail.com>
2014-06-17 10:28:06 +0100

    Updated home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @images = Image.last(24).reverse
+    @images = Image.where(owner_type: ["Camera", "Manufacturer"]).last(24).reverse
   end
   def about_cambase
   end
