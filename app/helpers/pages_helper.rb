3f0e60d923fa7c18de7c3ea0757a194e5bf8352c
shakeelanjum <shakeelmanjum@gmail.com>
2015-01-06 15:37:14 +0500

    Added models, recorders count on index page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,4 +2,8 @@ module PagesHelper
   def link_to_model_url(name = model.model, model)
     link_to(name, "/#{model.vendor.vendor_slug}/models/#{model.model_slug}")
   end
+
+  def models_count(name = model.model, model)
+    link_to(name, "/#{model.vendor.vendor_slug}/models/#{model.model_slug}")
+  end
 end
